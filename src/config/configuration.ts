b2f3937c95512646b1e915a43b93c5a57af180db
jayce.thai <phat.tnh@carehealth.io>
2025-03-21 10:39:02 +0700

    Init project

--- src/config/configuration.ts
+++
@@ -1,5 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('configuration', () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-}));
--- src/config/jwt.ts
+++
@@ -1,5 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('jwt', () => ({
-  secrectKey: process.env.SECRET_KEY,
-}));
